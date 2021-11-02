2cec974ccdfc4027ed74c83fb2639aa244249d17
Andrei Ivanou <andrei.ivanou@Kaltura.com>
2021-11-02 19:45:50 +0300

    Admin 1.0.0: Initial commit

---
+++ Admin/OSSOBase.Admin.ClientApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OSSOBase.Admin.ClientApp.Views;
+using System.IO;
+using System.Windows;
+
+namespace OSSOBase.Admin.ClientApp
+{
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public IConfiguration Configuration { get; private set; }
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+    }
+}
---
+++ Admin/OSSOBase.Admin.ClientApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace OSSOBase.Admin.ClientApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
